ae3abf839f03db2b1d85bc7ff5103891f864c25c
NguyenBaHoangKim <baloctq@gmail.com>
2024-05-17 15:05:56 +0700

    first commit

---
+++ src/main/java/com/KKApplication/KK/KKApplication.java
@@ -0,0 +1,13 @@
+package com.KKApplication.KK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KKApplication/KK/KKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KKApplication.KK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
